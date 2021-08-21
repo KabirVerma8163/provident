17e35e0c80d2a22bdaa2055922c0561ea58797af
toastedwaffle <kabir.verma.8163>
2021-08-20 22:46:35 -0400

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Dosis': ['Dosis', 'sans-serif'],
+      'Lato': ['Lato', 'sans-serif'],
+      'Montserrat': ['Montserrat', 'sans-serif'],
+      'Montserrat Alternates': ['Montserrat Alternates', 'sans-serif'],
+      'Roboto': ['Roboto', 'sans-serif'],
+      'Roboto Condensed': ['Roboto Condensed', 'sans-serif'],
+      'Ubuntu': ['Ubuntu', 'sans-serif'],
+      'Ubuntu Condensed': ['Ubuntu Condensed', 'sans-serif'],
+      'Ubuntu Mono': ['Ubuntu Mono', 'sans-serif'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
