1af7d1ba91f815fb2aa27279f42be31a10eef832
toastedwaffle <kabir.verma.8163>
2021-08-21 11:20:29 -0400

    Navbar fully operational

--- public/navbar.js
+++ public/navbar.js
@@ -1,9 +1,12 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+let lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){
+  if (window.pageYOffset < lastScrollTop){
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.top = "-80px";
   }
-}
+  lastScrollTop = window.pageYOffset
+  // document.getElementById("navbar").style.top = "-100px";
+  // document.getElementById("navbar").classList.add('bg-green-400')
+}, false);
