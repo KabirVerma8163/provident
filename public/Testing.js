8cffc03eba8284b2ce46dd66b5a13d1d84602622
Dongtai <dongtaishi.w@gmail.com>
2021-08-21 18:41:10 -0400

    asdf

--- public/Testing.js
+++ public/Testing.js
@@ -5,7 +5,7 @@ let token;
     base = 'https://sandbox.iexapis.com/stable/stock/';
     token = '?token=Tsk_7124566e8c6147939d1708c99bd3b78a';
     Highcharts.theme = {
-        colors: ['#21bd35', '#000000', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+        colors: ['#3b6c93', '#000000', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
             '#FF9655', '#FFF263', '#6AF9C4'],
         chart: {
             backgroundColor: '#0f2634',
@@ -63,22 +63,17 @@ async function getList(listType)
     return list;
 }
 
-function bruh (callback)
-{
-    getList({
-        success: function(response)
-        {
-            callback(response)
-        }
-    });
-}
-
 async function getListAndUpdateGraphs(listType, chartType, timeRange, elementNames)
 {
     let list = await getList(listType);
     for (let i = 0; i < elementNames.length; i++)
     {
         setTimeout(function () { createCandlestickGraph(list[i], chartType, timeRange, elementNames[i])}, i * 300);
+    }
+    var othergraphs = [document.getElementById("graph2"),document.getElementById("graph3"),document.getElementById("graph4"),document.getElementById("graph5")];
+    for (let i in othergraphs)
+    {
+        i.style.display = 'block';
     }
 }
 
@@ -190,6 +185,35 @@ function createCandlestickGraph(symbol, type, timeRange, elementName)
             }
         });
     });
+}
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+function searchSymbol()
+{
+    let symbol = document.getElementById("ticker").value;
+    createCandlestickGraph(symbol, 'chart', '1y', 'chart1');
+    var othergraphs = [document.getElementById("graph2"),document.getElementById("graph3"),document.getElementById("graph4"),document.getElementById("graph5")];
+    for (let i in othergraphs)
+    {
+        i.style.display = 'none';
+    }
 }
 /*http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
