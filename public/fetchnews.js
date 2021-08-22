5b3ee3294160d47bc4ffb63a9f2cf6e8537d15d2
Kevin Liu <kevinliu676@yahoo.ca>
2021-08-21 20:04:43 -0400

    news page

--- public/fetchnews.js
+++ public/fetchnews.js
@@ -33,13 +33,37 @@ async function fetchNews(ticker)
 (async function()
 {
     await fetchNews(ticker);
+
     document.getElementById("headline1").innerHTML = headlines[0];
     document.getElementById("summary1").innerHTML = summaries[0];
     document.getElementById("source1").innerHTML = sources[0];
-    document.getElementById("url1").innerHTML = urls[0];
-    document.getElementById("image1").innerHTML = images[0];
-    // console.log(headlines[0]);
-    // console.log(summaries[0]);
+    document.getElementById("url1").setAttribute("href",urls[0]);
+    document.getElementById("image1").src = images[0];
+
+    document.getElementById("headline2").innerHTML = headlines[1];
+    document.getElementById("summary2").innerHTML = summaries[1];
+    document.getElementById("source2").innerHTML = sources[1];
+    document.getElementById("url2").setAttribute("href",urls[1]);
+    document.getElementById("image2").src = images[1];
+
+    document.getElementById("headline3").innerHTML = headlines[2];
+    document.getElementById("summary3").innerHTML = summaries[2];
+    document.getElementById("source3").innerHTML = sources[2];
+    document.getElementById("url3").setAttribute("href",urls[2]);
+    document.getElementById("image3").src = images[2];
+
+    document.getElementById("headline4").innerHTML = headlines[3];
+    document.getElementById("summary4").innerHTML = summaries[3];
+    document.getElementById("source4").innerHTML = sources[3];
+    document.getElementById("url4").setAttribute("href",urls[3]);
+    document.getElementById("image4").src = images[3];
+
+    document.getElementById("headline5").innerHTML = headlines[4];
+    document.getElementById("summary5").innerHTML = summaries[4];
+    document.getElementById("source5").innerHTML = sources[4];
+    document.getElementById("url5").setAttribute("href",urls[4]);
+    document.getElementById("image5").src = images[4];
+
 })();
 
 
